cbe460464159e6e1b63b7cfcb2f0507940de626a
Rogoberto Ribeiro <usbetin@gmail.com>
2024-02-19 01:51:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ var dados = [];
 // Variável global para armazenar a soma dos valores positivos por mês e dia
 var somaPorMesDia = {};
 var subtracaoPorMesDia = {};
+// Inicialização de variáveis globais
+var somaPorMesDia = {}; // Criando um objeto vazio para armazenar a soma dos valores positivos por mês e dia
+var subtracaoPorMesDia = {}; // Criando um objeto vazio para armazenar a subtração dos valores negativos por mês e dia
+var faturamentoTotal = 0; // Variável global para armazenar o faturamento total
+var minimoDoMes = Infinity; // Variável global para armazenar o mínimo do mês
+var maximoDoMes = -Infinity; // Variável global para armazenar o máximo do mês
+var mediaDoMes = 0; // Variável global para armazenar a média do mês
+var faturamentoTotalSaida = 0; // Variável global para armazenar o faturamento total de saídas
+var minimoDoMesSaida = Infinity; // Variável global para armazenar o mínimo do mês de saídas
+var maximoDoMesSaida = -Infinity; // Variável global para armazenar o máximo do mês de saídas
+var mediaDoMesSaida = 0; // Variável global para armazenar a média do mês de saídas
 
 // Função para converter um número para o formato de moeda brasileira
 function formatarNumeroParaBRL(valor) {
@@ -25,7 +36,8 @@ function converterMatriz() {
   var saldo = 0;
 
   // Cria a tabela HTML
-  var tableHtml = '<table class="table table-bordered " border="1"  >';
+  var tableHtml = '<div class="table-responsive" ><table class="table table-striped table-borderless table-sm">';
+  tableHtml += '<tr><td>Mês</td> <td>Classificação</td> <td>Descrição</td> <td>Valor</td><td>Saldo</td> </tr>';
 
   // Itera sobre as linhas da matriz
   linhas.forEach(function (linha) {
@@ -72,7 +84,7 @@ function converterMatriz() {
   });
 
   // Fecha a tabela HTML
-  tableHtml += "</table>";
+  tableHtml += "</table></div>";
 
   // Exibe a tabela HTML na div de saída
   document.getElementById("outputDiv").innerHTML = tableHtml;
@@ -119,8 +131,9 @@ function tornarGraficoVisivel() {
 // Função para renderizar os dados na tabela
 function renderizarDados(somaPorMesDia) {
   var elementoDados = document.getElementById("dados");
-  var htmlTabela = '<table border="1">';
-  htmlTabela += "<tr><th>Mês/Ano</th><th>Total de Valores Positivos</th></tr>";
+  var htmlTabela = '<table border="1" class="table">';
+  var htmlTabela = '<div class="table-responsive" ><table class="table table-striped table-borderless  table-sm">';
+  htmlTabela += '<tr><td>Mês</td> <td>Entrada</td> <td>Saida</td> <td>Saldo</td> </tr>';
 
   for (var mesAno in somaPorMesDia) {
     var totalAno = 0;
@@ -130,15 +143,18 @@ function renderizarDados(somaPorMesDia) {
 
     var [mes, ano] = mesAno.split("/");
     var chave = mes + "/" + ano;
-    var valor_mes = somaPorMesDia[chave].toFixed(2);
+    var valor_mes = somaPorMesDia[chave];
+    var saida_mes = subtracaoPorMesDia[chave];
 
     htmlTabela += "<tr>";
     htmlTabela += "<td>" + chave + "</td>";
-    htmlTabela += "<td>" + valor_mes + "</td>";
+    htmlTabela += "<td>" + formatarNumeroParaBRL(valor_mes) + "</td>";
+    htmlTabela += "<td>" + formatarNumeroParaBRL(saida_mes) + "</td>";
+    htmlTabela += "<td>" + formatarNumeroParaBRL(valor_mes-saida_mes) + "</td>";
     htmlTabela += "</tr>";
   }
 
-  htmlTabela += "</table>";
+  htmlTabela += "</table></div>";
   elementoDados.innerHTML = htmlTabela;
 }
 
@@ -146,9 +162,9 @@ function renderizarDados(somaPorMesDia) {
 function criarGrafico() {
   tornarOutputInvisivel();
   renderizarGrafico(somaPorMesDia, subtracaoPorMesDia);
+  renderizarDados(somaPorMesDia)
 }
 
-// Função para renderizar o gráfico
 function renderizarGrafico(somaPorMesDia, subtracaoPorMesDia) {
   tornarGraficoVisivel();
   var ctx = document.getElementById("grafico").getContext("2d");
@@ -204,7 +220,88 @@ function renderizarGrafico(somaPorMesDia, subtracaoPorMesDia) {
     "subtracaoPorMesDia",
     JSON.stringify(subtracaoPorMesDia)
   );
+  gerarTabela(valores);
+  gerarTabelaSaidas(valoresSaidas)
 }
+
 
 // Torna o gráfico invisível ao carregar a página
 tornarGraficoInvisivel();
+
+
+function gerarTabela(valores) {
+  // Calcula a soma de todos os valores
+  var soma = valores.reduce((acc, curr) => acc + Number(curr), 0);
+  
+  
+    // Calcula o maior e o menor valor
+    var maior = Math.max(...valores);// Filtra os valores para remover os zeros
+  var valoresDiferentesDeZero = valores.filter(valor => valor !== 0);
+  
+  // Calcula o menor valor diferente de zero
+  var menor = Math.min(...valoresDiferentesDeZero);
+  
+    // Calcula a média dos valores
+    var media = soma / valores.length;
+    var elementoDados = document.getElementById("tabela2");
+    var htmlTabela = '<div class="table-responsive" ><table class="table table-striped table-borderless table-sm">';
+  
+  
+      htmlTabela += "<tr>";
+      htmlTabela += "<td> Total </td>";
+      htmlTabela += "<td>" + formatarNumeroParaBRL(soma) + "</td>";
+      htmlTabela += "</tr>";
+      htmlTabela += "<tr>";
+      htmlTabela += "<td> Maior </td>";
+      htmlTabela += "<td>" + formatarNumeroParaBRL(maior) + "</td>";
+      htmlTabela += "</tr>";
+      htmlTabela += "<tr>";
+      htmlTabela += "<td> Menor </td>";
+      htmlTabela += "<td>" + formatarNumeroParaBRL(menor) + "</td>";
+      htmlTabela += "</tr>";
+      htmlTabela += "<tr>";
+      htmlTabela += "<td> Média </td>";
+      htmlTabela += "<td>" + formatarNumeroParaBRL(media) + "</td>";
+      htmlTabela += "</tr>";
+  
+    htmlTabela += "</table></div>";
+    elementoDados.innerHTML = htmlTabela;
+  }
+  function gerarTabelaSaidas(valores) {
+  // Calcula a soma de todos os valores
+  var soma = valores.reduce((acc, curr) => acc + Number(curr), 0);
+  
+  
+    // Calcula o maior e o menor valor
+    var maior = Math.max(...valores);// Filtra os valores para remover os zeros
+  var valoresDiferentesDeZero = valores.filter(valor => valor !== 0);
+  
+  // Calcula o menor valor diferente de zero
+  var menor = Math.min(...valoresDiferentesDeZero);
+  
+    // Calcula a média dos valores
+    var media = soma / valores.length;
+    var elementoDados = document.getElementById("tabela3");
+    var htmlTabela = '<div class="table-responsive" ><table class="table table-striped table-borderless table-sm">';
+  
+  
+      htmlTabela += "<tr>";
+      htmlTabela += "<td> Total </td>";
+      htmlTabela += "<td>" + formatarNumeroParaBRL(soma) + "</td>";
+      htmlTabela += "</tr>";
+      htmlTabela += "<tr>";
+      htmlTabela += "<td> Maior </td>";
+      htmlTabela += "<td>" + formatarNumeroParaBRL(maior) + "</td>";
+      htmlTabela += "</tr>";
+      htmlTabela += "<tr>";
+      htmlTabela += "<td> Menor </td>";
+      htmlTabela += "<td>" + formatarNumeroParaBRL(menor) + "</td>";
+      htmlTabela += "</tr>";
+      htmlTabela += "<tr>";
+      htmlTabela += "<td> Média </td>";
+      htmlTabela += "<td>" + formatarNumeroParaBRL(media) + "</td>";
+      htmlTabela += "</tr>";
+  
+    htmlTabela += "</table></div>";
+    elementoDados.innerHTML = htmlTabela;
+  }
